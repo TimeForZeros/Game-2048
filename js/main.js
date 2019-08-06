06b475941776211a61e4912abf56ffa6dd5facfa
Victor Gonzalez <dazedbutamused@gmail.com>
2019-08-06 13:16:12 -0700

    all moves work

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const colors = {
     '0'   : 'white',
-    '2'   : 'RGBA(0, 0, 0, 0.1)',
-    '4'   : 'RGBA(0, 0, 0, 0.2)',
-    '8'   : 'RGBA(0, 0, 0, 0.3)',
-    '16'  : 'RGBA(0, 0, 0, 0.4)',
+    '2'   : 'RGBA(0, 0, 50, 0.1)',
+    '4'   : 'RGBA(0, 25, 50, 0.2)',
+    '8'   : 'RGBA(0, 50, 50, 0.3)',
+    '16'  : 'RGBA(0, 50, 100, 0.4)',
     '32'  : 'RGBA(0, 0, 0, 0.5)',
     '64'  : 'RGBA(0, 0, 0, 0.6)',
     '128' : 'RGBA(0, 0, 0, 0.7)',
     '256' : 'RGBA(0, 0, 0, 0.8)',
     '512' : 'RGBA(0, 0, 0, 0.9)',
-    '1024': 'RGBA(0, 0, 0, 1.0)',
+    '1024': 'RGBA(150, 100, 100, 1.0)',
     '2048': 'RGBA(255, 0, 0, 1)'
 }
 
@@ -30,11 +30,20 @@ const moves = {
     },
     up: function shiftUp(boardArr) {
         boardArr = colConverter(boardArr);
-        boardArr.forEach(function (arr, idx)
-        { boardArr[idx] = leftMove(arr);
-            return  boardArr[idx];
-        });
-        boardArr = colConverter(boardArr);
+            boardArr.forEach(function (arr, idx)
+            { boardArr[idx] = leftMove(arr);
+                return  boardArr[idx];
+            });
+            boardArr = colConverter(boardArr);
+            return boardArr;
+
+
+        // boardArr = colConverter(boardArr);
+        // boardArr.forEach(function (arr, idx)
+        // { boardArr[idx] = leftMove(arr);
+        //     return  boardArr[idx];
+        // });
+        // boardArr = colConverter(boardArr);
         return boardArr;
     },
     down: function shiftDown(boardArr) {
@@ -101,26 +110,45 @@ function leftMove(arr) {
  return fillArr(combineNums(reduceArr(arr)));
 }
 
-//create column arrays converter
+//updated Col Converter
+
 function colConverter(arr) {
-    var colOne =[], colTwo = [], colThree = [], colFour = [];
-    var colArrays = [colOne, colTwo, colThree, colFour];
-    arr.forEach(function(idxArr){
-        colOne.push(idxArr[0]);
-        colTwo.push(idxArr[1]);
-        colThree.push(idxArr[2]);
-        colFour.push(idxArr[3]);
-        return colArrays;
+var transposedArr = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]];
+arr.forEach(function(rowArr, rowIdx) {
+    rowArr.forEach(function(val, colIdx) {
+        transposedArr[colIdx].splice(rowIdx, 1, val);
+        return transposedArr;
     });
-return colArrays;
-    }
+    return transposedArr;
+});
+return transposedArr;
+}
+
+
+//create column arrays converter
+// function colConverter(arr) {
+//     var colOne =[], colTwo = [], colThree = [], colFour = [];
+//     var colArrays = [colOne, colTwo, colThree, colFour];
+//     arr.forEach(function(idxArr){
+//         colOne.push(idxArr[0]);
+//         colTwo.push(idxArr[1]);
+//         colThree.push(idxArr[2]);
+//         colFour.push(idxArr[3]);
+//         return colArrays;
+//     });
+// return colArrays;
+//     }
 //revert column array by performing this function again
 
 //console.log(moves.down(board));
 
 function init() {
-    render();
-    spawnInit();
+    render(board);
+    spawnInit(board);
     console.log(board);
 }
 
@@ -129,7 +157,7 @@ function init() {
 
 //creating the render function
 
-function render () {
+function render(board) {
     //function that goes through the board array rows
     board.forEach(function(colArr, rowIdx) {
         //function that goes through the column index of the row arrays
@@ -139,6 +167,7 @@ function render () {
             
         });
     });
+    return board;
 }
 
 
@@ -148,7 +177,7 @@ function render () {
 function randomNum() {
     return (Math.ceil(Math.random() * 4) - 1);
 }
-render();
+render(board);
 
 function renderUpdate() {
     let col = randomNum();
@@ -166,13 +195,13 @@ function renderUpdate() {
     }
     }
     else {
-    renderUpdate();
+    renderUpdate(board);
     }
     updateNum = board[row][col];
     return tileUpdate.style.backgroundColor = colors[updateNum.toString()];
 }
 
-function spawnInit () {
+function spawnInit (board) {
     for (var i = 0; i < 2;) {
         let col = randomNum();
         let row = randomNum();
@@ -194,35 +223,23 @@ function spawnInit () {
 document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 37: //left
-        {
-            moves.left(board);
-            renderUpdate();
-            render();
+        {      
+         return   renderUpdate(render(moves.left(board)));
             console.log(board);
         }
         break;
         case 38: //up
         {
-            moves.up(board);
-            renderUpdate();
-            render();
-            console.log(board);
-        }
+          return  renderUpdate(render(moves.up(board)));        }
         break;
         case 39: //right
         {
-            moves.right(board);
-            renderUpdate();
-            render();
-            console.log(board);
+          return  renderUpdate(render(moves.right(board)));
         }
         break;
         case 40: //down
         {
-            moves.down(board);
-            renderUpdate();
-            render();
-            console.log(board);
+            return renderUpdate(render(moves.down(board)));
         }
             break;
     }
